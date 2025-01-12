ab432c5a62500eb40944e9f1fa7eafa9c5ffe444
Rodrigo Martins de Sousa <rodrigo.msousa001@gmail.com>
2025-01-12 17:09:33 -0300

    primeiro commit

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+
+        private Evento[] _evento = new Evento[] {
+                new Evento() {
+                EventoId = 1,
+                Tema = "Angular 11 e .NET Core 5",
+                Local = "Belo Horizonte",
+                Lote = "1º Lote",
+                QtdPesoas = 250,
+                DataEvento = DateTime.Now.AddDays(2).ToString(),
+                ImagemURL = "foto.png"
+                },
+                new Evento() {
+                EventoId = 2,
+                Tema = "Angular e suas novidades",
+                Local = "São Paulo",
+                Lote = "2º Lote",
+                QtdPesoas = 350,
+                DataEvento = DateTime.Now.AddDays(3).ToString(),
+                ImagemURL = "foto2.png"
+                }
+        };
+
+        public EventoController()
+        {
+
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _evento;
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> GetById(int id)
+        {
+            return _evento.Where(evento => evento.EventoId == id);
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "Exemplo de post";
+        }     
+
+        [HttpPut("{id}")]
+        public string Put(int id)
+        {
+            return $"Exemplo de put com id = {id}";
+        }  
+
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            return $"Exemplo de delete com id = {id}";
+        }               
+    }
+}
